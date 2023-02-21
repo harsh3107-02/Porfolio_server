7ba789b96c070363cbbc83d5716dce9d5c3fb442
Harsh <harshag2468@gmail.com>
2023-02-21 22:05:56 +0530

    correct

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.listen(5000, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
     service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: "harshag2468@gmail.com",
         pass: "vzneawjlozvymgf"
